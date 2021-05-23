6fb872d74b5b9d39ae73f216104005bb816895d5
theopmw <theopmw@gmail.com>
2021-05-23 12:28:53 +0000

    Center contact form  modals vertically, add comments

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -6,20 +6,21 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            document.getElementById("form").reset();
-            $('.success-modal').modal('show');
+            document.getElementById("form").reset(); // Reset form
+            $('.success-modal').modal('show'); // Show success modal
         }, function(error) {
-            $('.error-modal').modal('show');
+            $('.error-modal').modal('show'); // Show error modal
         })
         return false;
 };
 
+// Vertically align modal
 // Credit: code taken from https://www.tutorialrepublic.com/faq/how-to-align-bootstrap-modal-vertically-center.php
 $(document).ready(function(){
     function alignModal(){
         var modalDialog = $(this).find(".modal-dialog");
         
-        // Applying the top margin on modal to align it vertically center
+        // Apply top margin on modal to align it vertically center
         modalDialog.css("margin-top", Math.max(0, ($(window).height() - modalDialog.height()) / 2));
     }
     // Align modal when it is displayed
